9212c624b56d47eef02078ccee1459a88b74393e
seif <seif.m05@gmail.com>
2024-11-25 00:51:39 +0200

    project start

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //importing packages
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+
 var app = express();
 
 // view engine setup
@@ -14,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //our code is here
+/*
 app.get('/',function(req,res){
   res.render('index',{title:"express"})
 });
@@ -23,6 +26,34 @@ app.post('/',function(req,res){
   data = req.body.user;
   console.log(data);
   res.render('index',{title:"express"})
+});
+*/
+
+app.get('/',function(req,res){
+  res.render('login')
+});
+//working with JSONs
+/*
+let x = {name:'Seif',username:'user1',pass:"pass1"};
+let y = JSON.stringify(x);
+let z = JSON.parse(y);
+console.log(x);
+console.log(y);
+console.log(z);
+//write into json file
+fs.writeFileSync('users.json',y);
+let data = fs.readFileSync('users.json')
+let dataReturned = JSON.parse(data)
+console.log(dataReturned)
+*/
+//connecting to MongoDB
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect("mongodb://localhost:27017",function(err,client){
+  if(err){
+    throw err;
+  }
+  var db = client.db('TestDB');
+  db.collection('FirstCollection').insertOne({id:1,firstName:'Seif',lastName:'Hamdy'});
 });
 
 //the port
